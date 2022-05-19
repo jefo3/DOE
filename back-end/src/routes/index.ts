0e70dd0572716686e43f61c369c063d71e556e98
Jeferson Goncalves <jeferson.goncalves.noronha@gmail.com>
2022-05-19 01:40:42 -0300

    feat: add conecçao com o banco

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', async (request, response) => {
-  return response.json({ teste: 'teste' });
+  return response.json({ teste: 'teste111' });
 });
 
 export default routes;
