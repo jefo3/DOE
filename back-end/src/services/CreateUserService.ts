38c80508f0ed6b3d45a02cc63ddc3f88fcce429b
Jeferson Goncalves <jeferson.goncalves.noronha@gmail.com>
2022-05-21 13:33:30 -0300

    feat: service create user

---
+++ back-end/src/services/CreateUserService.ts
@@ -0,0 +1,44 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({
+    name,
+    surname,
+    email,
+    password,
+  }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const checkUserExist = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExist) {
+      throw new Error('email address already used');
+    }
+    const hashPassword = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      surname,
+      email,
+      password: hashPassword,
+    });
+
+    userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
