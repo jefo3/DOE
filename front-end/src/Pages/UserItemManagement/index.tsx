8182621daee391999f14db0bb421a9caa3c0e36b
FelipeRaulino <felipe.raaulino29@gmail.com>
2022-05-24 13:49:30 -0300

    recuperando dados do back-end na tela de gerenciamento de doações do usuário.

--- front-end/src/Pages/UserItemManagement/index.tsx
+++ front-end/src/Pages/UserItemManagement/index.tsx
@@ -1,35 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { AiFillCheckCircle, AiFillMinusCircle } from 'react-icons/ai';
 import { BsPencilFill } from 'react-icons/bs';
 
 import NavMenu from '../../Components/NavMenu';
 import SidebarMenu from '../../Components/SidebarMenu';
+import { IDonate } from '../../Store/Interfaces/donateInterfaces';
+import { getDonatesByIdUser } from '../../Store/Services/donateServices';
 
 import { Container, Content, MainContent, DonationItem, IconsMenu } from './styles';
 
 const UserItemManagement: React.FC = () => {
-  return (
-    <Container>
-        <NavMenu />
-        <Content>
-            <SidebarMenu />
-            <MainContent>
-                <h1>Gerenciamento de itens</h1>
-                <DonationItem>
-                    <span>Camisa polo</span>
-                    <span>Roupa</span>
-                    <span>11/04/2022</span>
-                    <IconsMenu>
-                        <AiFillMinusCircle color="#EE3A3A" /> 
-                        <BsPencilFill color="#2E69C2" />
-                        <AiFillCheckCircle color="#019006" />
-                    </IconsMenu>
-                </DonationItem>
-            </MainContent>
-        </Content>
-    </Container>
-);
+
+    const [userDonations, setUserDonations] = useState<Array<IDonate>>();
+
+    const retrievingUserDonations = () => {
+        try {
+            return getDonatesByIdUser().then(response => {
+                setUserDonations(response);
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        retrievingUserDonations();
+    }, []);
+
+    return (
+        <Container>
+            <NavMenu />
+            <Content>
+                <SidebarMenu />
+                <MainContent>
+                    <h1>Gerenciamento de itens</h1>
+                    {
+                        userDonations?.map(userDonation => (
+                            <DonationItem key={userDonation.id}>
+                                <span>{userDonation.title}</span>
+                                <span>{userDonation.tag.name}</span>
+                                <span>{userDonation.created_at}</span>
+                                <IconsMenu>
+                                    <AiFillMinusCircle color="#EE3A3A" /> 
+                                    <BsPencilFill color="#2E69C2" />
+                                    <AiFillCheckCircle color="#019006" />
+                                </IconsMenu>
+                            </DonationItem>
+                        ))
+                    } 
+                </MainContent>
+            </Content>
+        </Container>
+    );
 }
 
 export default UserItemManagement;
--- front-end/src/Store/Services/donateServices.ts
+++ front-end/src/Store/Services/donateServices.ts
@@ -7,8 +7,8 @@ export const createDonate = (donate: ICreatDonate) =>{
     return apiPost<ICreatDonate>(DONATES_ROUTER, donate);
 }
 
-export const getDonatesByIdUser = (idUser: string) =>{
-    return apiGet<Array<IDonate>>(DONATES_ROUTER+'/'+idUser);
+export const getDonatesByIdUser = () =>{
+    return apiGet<Array<IDonate>>(DONATES_ROUTER);
 }
 
 export const getAllDonates = () => {
