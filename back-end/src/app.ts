0f049b896e3dddd7be34868322c37074de336f98
Jeferson Goncalves <jeferson.goncalves.noronha@gmail.com>
2022-06-28 14:20:38 -0300

    feat: servindo os arquivos de forma statica

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -5,12 +5,14 @@ import cors from 'cors';
 import routes from './routes';
 import './database';
 import './database';
-import bodyParser from 'body-parser';
+import uploadConfig from './config/upload';
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/files', express.static(uploadConfig.directory))
 app.use(routes);
 
 export default app;
