0f9dd1cd7485584e0cd991b35ef65891bf7b63fa
Jeferson Goncalves <jeferson.goncalves.noronha@gmail.com>
2022-06-29 11:14:24 -0300

    test: ajuste no teste de criaçao

--- back-end/test/donate/DonateCreate.spec.ts
+++ back-end/test/donate/DonateCreate.spec.ts
@@ -1,7 +1,7 @@
 import CreateDonateService from "../../src/services/donate/CreateDonateService"
 import { donateRepositoryInMemory } from "./mocks/CreateDonateMock";
 
-describe('Donate', () => {
+describe('Create donate', () => {
   let createDonateService: CreateDonateService;
 
   beforeEach(() => {
@@ -62,5 +62,36 @@ describe('Donate', () => {
     expect(donateRepositoryInMemory.save).toHaveBeenCalled();
     expect(donateRepositoryInMemory.create).toHaveBeenCalledTimes(1);
     expect(donateRepositoryInMemory.save).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not create a new donate', async () => {
+    const donateInputMock = {
+      title: "farofa 2",
+      description: "feijao gostoso demais bem demais",
+      user_id: "50404b0f-8f1b-4081-9436-da454bf110d2",
+      tag_id: "b5e67b99-8468-4525-a9e6-a6fb8a591538",
+      status_donate: "doado",
+    };
+
+    const donateOutputMock = {
+      title: "farofa 2",
+      description: "feijao gostoso demais bem demais",
+      user_id: "50404b0f-8f1b-4081-9436-da454bf110d2",
+      tag_id: "b5e67b99-8468-4525-a9e6-a6fb8a591538",
+      status_donate: "doado",
+      id: "8e9c96dd-2299-4aca-b4c5-2938d240cc5d",
+      created_at: "2022-05-24T18:30:54.376Z",
+      updated_at: "2022-05-24T18:30:54.376Z"
+
+    }
+
+    donateRepositoryInMemory.create.mockReturnValue(Promise.resolve(donateOutputMock));
+    donateRepositoryInMemory.save.mockImplementationOnce(() => Promise.reject());
+
+    try{
+      await createDonateService.execute({ ...donateInputMock });
+    }catch(error){
+      expect((error as Error).message).toBe('Internal server error, please try again');
+    }
   });
 });
